07939a41e533c67edc297141acd57f0940fe531d
gitgurra <55788928+gitgurra@users.noreply.github.com>
2020-03-13 10:50:26 +0100

    Update CentroidTracker.py
    Modifierad att spåra mitten på nedre kant istället för centroiden.

--- Sammanslagning/CentroidTracker.py
+++ Sammanslagning/CentroidTracker.py
@@ -63,7 +63,7 @@ class CentroidTracker():
         for (i, (startX, startY, endX, endY)) in enumerate(rects):
             # use the bounding box coordinates to derive the centroid
             cX = int((startX + endX) / 2.0)
-            cY = int((startY + endY) / 2.0)
+            cY = startY
             inputCentroids[i] = (cX, cY)
 
         # if we are currently not tracking any objects take the input
